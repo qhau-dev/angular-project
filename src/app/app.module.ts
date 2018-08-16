607acb2d289779374bd0a7419df4f7126fd78984
Hau <quanghau96@gmail.com>
2018-08-16 23:25:50 +0700

    build stucture for pet project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { HomeComponent } from './main/home/home.component';
+import { FunctionComponent } from './main/function/function.component';
+import { ProductComponent } from './main/product/product.component';
+import { ProductCategoryComponent } from './main/product-category/product-category.component';
+import { RoleComponent } from './main/role/role.component';
+import { UserComponent } from './main/user/user.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    HomeComponent,
+    FunctionComponent,
+    ProductComponent,
+    ProductCategoryComponent,
+    RoleComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
